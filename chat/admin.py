343e5d9e4b01d5c1360301bd77a88b8859f80db1
Sascha Nemeth <sascha.nemeth@me.com>
2024-05-10 14:05:19 +0200

    adding chat to message

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Chat, Message
 
+class MessageAdmin (admin.ModelAdmin):
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'receiver', 'text')
+    search_fields = ('text',)
+ 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
