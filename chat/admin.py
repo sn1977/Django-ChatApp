23e4f4de4d8e4f48cdf91526b3013b04fb6d202a
Sascha Nemeth <sascha.nemeth@me.com>
2024-05-10 14:46:54 +0200

    create object in db

--- chat/admin.py
+++ chat/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Chat, Message
 
-class MessageAdmin (admin.ModelAdmin):
-    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
-    list_display = ('created_at', 'author', 'receiver', 'text')
-    search_fields = ('text',)
- 
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ("chat", "text", "created_at", "author", "receiver")
+    list_display = ("created_at", "author", "receiver", "text")
+    search_fields = ("text",)
+
+
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
 admin.site.register(Chat)
--- chat/models.py
+++ chat/models.py
@@ -5,15 +5,30 @@ from django.conf import settings
 
 # Create your models here.
 
+
 class Chat(models.Model):
     created_at = models.DateField(default=date.today)
+
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
     # chat = Chat Klasse verknüpfen
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
-  
-
-
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+        related_name="chat_message_set",
+        default=None,
+        blank=True,
+        null=True,
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="author_message_set",
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="receiver_message_set",
+    )
