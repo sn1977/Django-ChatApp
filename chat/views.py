fdd43a6b871a99c327bef720e5dc05c2568dc0a3
Sascha Nemeth <sascha.nemeth@me.com>
2024-05-12 12:40:21 +0200

    adding docs, ja, and more

--- chat/views.py
+++ chat/views.py
@@ -1,22 +1,28 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core import serializers
 
 
 @login_required(login_url="/login/")
 def index(request):
+    """
+    This is a view to render the chat html (ist ein Beispiel).
+    """
     if request.method == "POST":
         print("Received data " + request.POST["textmessage"])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(
+        new_message = Message.objects.create(
             text=request.POST["textmessage"],
             chat=myChat,
             author=request.user,
             receiver=request.user,
         )
+        serialized_obj = serializers.serialize('json', [new_message])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, "chat/index.html", {"messages": chatMessages})
 
@@ -41,17 +47,20 @@ def login_view(request):
 
 def register_view(request):
     if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
         username = request.POST.get("username")
+        email = request.POST.get("email")
         password = request.POST.get("password")
         # Hier kannst du weitere Felder aus dem Formular abrufen und dem Benutzer zuweisen
 
         # Benutzer erstellen und in die Datenbank speichern
         user = User.objects.create_user(username, password=password)
         # Weitere Benutzerattribute setzen, falls erforderlich
-        # user.first_name = "John"
-        # user.last_name = "Doe"
-        # user.email = "john@example.com"
-        # user.save()
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
 
         # Optional: Weiterleitung nach erfolgreicher Registrierung
         return redirect('login/')  # Zum Beispiel zur Login-Seite
---
+++ docs/_build/html/_sources/modules.rst.txt
@@ -0,0 +1,9 @@
+django_chat_app
+===============
+
+.. toctree::
+   :maxdepth: 4
+
+   chat
+   first_django_app
+   manage
